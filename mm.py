a516a74553828bdc7b4097f3554f6099d0bb884d
khalil <bedouimohamedkhamim@gmail.com>
2024-12-14 11:47:21 +0100

    Adjustes

---
+++ mm.py
@@ -0,0 +1,7 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+n = np.array(np.random.randint(1, 10))
+m = np.array(np.random.randint(1, 10))
+plt.scatter(n, m, c='r')
+plt.show()
